b96b534f204765ee4c565669ce7ab67b1a48a498
AngelCorona <aangelcoronaa@gmail.com>
2026-02-25 17:05:00 -0600

    Server Node
    ----

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  // Configuramos el texto para que acepte acentos y formato HTML
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  
+  res.end('<h1>Test Node Github</h1><p>Prueba Contenedor</p>');
+});
+
+// El puerto 3000 es clave porque es el que configuraste en tu docker-compose
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor Node listo ${PORT}`);
+});
