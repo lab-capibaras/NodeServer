05c020583d5674edcca44579a63fdcfe3c5f4140
Angel Corona <aangelcoronaa@gmail.com>
2026-02-25 17:36:02 -0600

    Prueba Push

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   // Configuramos el texto para que acepte acentos y formato HTML
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   
-  res.end('<h1>Test Node Github</h1><p>Prueba Contenedor</p>');
+  res.end('<h1>Test Node Github Prueba Push 1</h1><p>Prueba Contenedor</p>');
 });
 
 // El puerto 3000 es clave porque es el que configuraste en tu docker-compose
