b748f27439c74ef4eab628001adfea1573b85ef4
Angel Corona <aangelcoronaa@gmail.com>
2026-02-25 16:02:33 -0600

    Test push

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('NodeServer prueba github\n');
+  res.end('NodeServer prueba github 2 push\n');
 });
 server.listen(3000, () => console.log('Servidor listo'));
